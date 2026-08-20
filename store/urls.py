9c8db5999b1e5e544b98db9c95a5bb8d4ba4500f
gideonel <lordgideonel@gmail.com>
2026-08-20 13:33:04 +0100

    added api documentation

--- store/urls.py
+++ store/urls.py
@@ -16,8 +16,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+#Swagger and Redoc documentation
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Store Project API",
+        default_version='v1',
+        description="API documentation for the Store Project",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="lordgideonel@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('market/api/', include('market.urls')),
+    path('market/api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('market/api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
